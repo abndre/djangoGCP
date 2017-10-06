30bb6236138ba2c58b60abbd4792307e02471cfe
andre noite <andreziobarros@gmail.com>
2017-10-06 03:09:16 -0300

    novo banco player

--- meusite/meuapp/admin.py
+++ meusite/meuapp/admin.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 # Register your models here.
-from .models import Todo
+from .models import Todo, Player
 
 admin.site.register(Todo)
+admin.site.register(Player)
--- meusite/meuapp/models.py
+++ meusite/meuapp/models.py
@@ -5,10 +5,24 @@ from django.db import models
 
 # Create your models here.
 class Todo(models.Model):
-    title = models.CharField(max_length=200)
-    text = models.TextField()
-    creat_at =  models.DateTimeField(default = datetime.now, blank = True)
+    title        = models.CharField(max_length=200)
+    text         = models.TextField()
+    creat_at     =  models.DateTimeField(default = datetime.now, blank = True)
 
 
     def __str__(self):
         return self.title
+
+
+
+class Player(models.Model):
+    name        = models.CharField(max_length=200)
+    level       = models.IntegerField()
+    xp          = models.IntegerField()
+    ataque      = models.IntegerField()
+    defese      = models.IntegerField()
+    creat_at    =  models.DateTimeField(default = datetime.now, blank = True)
+
+
+    def __str__(self):
+        return self.name

36a9505603946b7b2c78462c4b47af905af0006a
Andrezio <andrezio@live.com>
2017-10-07 17:05:27 -0300

    atualizado aba jogo

--- meusite/bootsite/views.py
+++ meusite/bootsite/views.py
@@ -5,7 +5,24 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+from .models import Jogador
 
 def index(request):
     #return HttpResponse("Hello, world")
     return render(request, 'index.html')
+
+def menuplayer(request,id):
+
+    jogado = Jogador.objects.get(id=id)
+    context = {
+        'jogado':jogado
+    }
+    return render (request, 'details.html',context)
+
+
+def menu(request):
+    jogadores = Jogador.objects.all()#[:10]
+    context = {
+        'jogadores':jogadores
+    }
+    return render (request, 'players.html',context)
